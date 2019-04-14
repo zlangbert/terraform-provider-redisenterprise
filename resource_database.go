537fff0e9563a239e802e6c0bb9941ab7dc64fe2
Zach Langbert <zach.langbert@gmail.com>
2019-04-14 16:28:33 -0700

    implement exists, update, and delete

--- resource_database.go
+++ resource_database.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/pkg/errors"
 	redisenterprise "github.com/zlangbert/redis-enterprise-client-go"
@@ -10,6 +11,7 @@ import (
 
 func resourceDatabase() *schema.Resource {
 	return &schema.Resource{
+		Exists: resourceDatabaseExists,
 		Create: resourceDatabaseCreate,
 		Read:   resourceDatabaseRead,
 		Update: resourceDatabaseUpdate,
@@ -31,15 +33,58 @@ func resourceDatabase() *schema.Resource {
 			},
 			"memory_size": {
 				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"replication": {
+				Type:     schema.TypeBool,
 				Optional: true,
 			},
-			"shard_count": {
-				Type:     schema.TypeInt,
+			"sharding": {
+				Type:     schema.TypeMap,
 				Optional: true,
-				Default:  1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"enabled": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"shards": {
+							Type:     schema.TypeInt,
+							Optional: true,
+							Default:  1,
+						},
+						"placement": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Default:  "dense",
+						},
+					},
+				},
+				Default: map[string]interface{}{
+					"count":     1,
+					"placement": "dense",
+				},
 			},
 		},
 	}
+}
+
+func resourceDatabaseExists(d *schema.ResourceData, m interface{}) (bool, error) {
+	meta := m.(*providerMeta)
+	id, _ := strconv.Atoi(d.Id())
+
+	log.Printf("[DEBUG] checking for existence of database %v", id)
+	_, response, err := meta.client.DatabasesApi.GetDatabase(meta.ctx, int32(id))
+
+	if response.StatusCode == 200 {
+		return true, nil
+	}
+
+	if response.StatusCode == 404 {
+		return false, nil
+	}
+
+	return false, getClientError(err)
 }
 
 func resourceDatabaseCreate(d *schema.ResourceData, m interface{}) error {
@@ -55,16 +100,30 @@ func resourceDatabaseCreate(d *schema.ResourceData, m interface{}) error {
 	}
 
 	if v, ok := d.GetOk("memory_size"); ok {
-		db.MemorySize = int32(v.(int))
-	} else {
-		db.MemorySize = 0
+		db.MemorySize = int64(v.(int))
 	}
 
-	if v, ok := d.GetOk("shard_count"); ok {
-		db.ShardsCount = int32(v.(int))
+	if v, ok := d.GetOk("replication"); ok {
+		db.Replication = v.(bool)
 	}
 
-	log.Printf("[DEBUG] creating database with payload: %#v", db)
+	if v, ok := d.GetOk("sharding"); ok {
+		shardingConfig := v.(map[string]interface{})
+
+		shards, err := strconv.Atoi(shardingConfig["shards"].(string))
+		if err != nil {
+			return errors.Wrapf(err, "failed to parse sharding.shards value: %v", shardingConfig["shards"])
+		}
+
+		if shards > 1 {
+			db.Sharding = true
+		}
+		db.ShardsCount = int32(shards)
+		db.ShardsPlacement = shardingConfig["placement"].(string)
+	}
+
+	payload, _ := json.Marshal(db)
+	log.Printf("[DEBUG] creating database with payload: %s", payload)
 
 	database, _, err := meta.client.DatabasesApi.CreateDatabase(meta.ctx, db)
 	if err != nil {
@@ -80,7 +139,6 @@ func resourceDatabaseCreate(d *schema.ResourceData, m interface{}) error {
 
 func resourceDatabaseRead(d *schema.ResourceData, m interface{}) error {
 	meta := m.(*providerMeta)
-
 	id, _ := strconv.Atoi(d.Id())
 
 	database, _, err := meta.client.DatabasesApi.GetDatabase(meta.ctx, int32(id))
@@ -88,24 +146,67 @@ func resourceDatabaseRead(d *schema.ResourceData, m interface{}) error {
 		return errors.Wrapf(getClientError(err), "error getting database %v", id)
 	}
 
+	log.Printf("[DEBUG] read database %v: %#v", id, database)
+
 	_ = d.Set("name", database.Name)
 	_ = d.Set("type", database.Type)
 	_ = d.Set("port", database.Port)
 	_ = d.Set("memory_size", database.MemorySize)
-	_ = d.Set("shard_count", database.ShardsCount)
+	_ = d.Set("replication", database.Replication)
+
+	_ = d.Set("sharding", map[string]interface{}{
+		"enabled":   database.Sharding,
+		"shards":    int(database.ShardsCount),
+		"placement": database.ShardsPlacement,
+	})
 
 	return nil
 }
 
 func resourceDatabaseUpdate(d *schema.ResourceData, m interface{}) error {
+	meta := m.(*providerMeta)
+	id, _ := strconv.Atoi(d.Id())
+
+	db := redisenterprise.Database{}
+
+	if d.HasChange("name") {
+		db.Name = d.Get("name").(string)
+	}
+
+	if d.HasChange("memory_size") {
+		db.MemorySize = int64(d.Get("memory_size").(int))
+	}
+
+	if d.HasChange("replication") {
+		db.Replication = d.Get("replication").(bool)
+	}
+
+	payload, _ := json.Marshal(db)
+	log.Printf("[DEBUG] updating database with payload: %s", payload)
+
+	_, _, err := meta.client.DatabasesApi.UpdateDatabase(meta.ctx, int32(id), db)
+	if err != nil {
+		return errors.Wrapf(getClientError(err), "error updating database %v", id)
+	}
+
 	return resourceDatabaseRead(d, m)
 }
 
 func resourceDatabaseDelete(d *schema.ResourceData, m interface{}) error {
+	meta := m.(*providerMeta)
+	id, _ := strconv.Atoi(d.Id())
+
+	_, err := meta.client.DatabasesApi.DeleteDatabase(meta.ctx, int32(id))
+	if err != nil {
+		return errors.Wrapf(getClientError(err), "error deleting database %v", id)
+	}
+
 	return nil
 }
 
 func getClientError(err error) error {
+
+	log.Printf("[DEBUG] unwraping client error: %#v", err)
 
 	switch err := err.(type) {
 	default:
